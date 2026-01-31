8482c0139dbae86f2f6a48e12fa7cdda0ad4e95b
Vikas <Vikas@MacBook-Pro.local>
2026-01-31 10:27:26 +0530

    fix: embed ZK artifacts in code to bypass FS

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["snarkjs"],
-  experimental: {
-    // @ts-ignore
-    outputFileTracingIncludes: {
-      '/api/prove': ['./zk-artifacts/**/*'],
-    },
-  },
 };
 
 export default nextConfig;
